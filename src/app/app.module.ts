787c0280ffe4ba969d1952747ef89df139959d3c
RonnySerrano <serranoronny@yahoo.com>
2022-11-07 20:01:36 -0500

    primer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './assets/navbar/navbar.component';
+import { FooterComponent } from './assets/footer/footer.component';
+import { IndexComponent } from './assets/index/index.component';
+import { ErrorComponent } from './assets/error/error.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modelos/aeropuerto.model.ts
@@ -0,0 +1,11 @@
+export class AeropuertoModel{
+    id?: String;
+    nombre?: String;
+    ciudad?: string;
+    pais?: string;
+    coord_X?: string;
+    coord_Y?: string;
+    siglas?: string;
+    tipo?: string;
+  }
+  
